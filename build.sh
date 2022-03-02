0c56150706710afdcbd449e1e5e31f1de0acbdf9
qbxt <q@queue.bot>
2022-03-02 14:01:19 -0600

    add build script

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cowsay "Building Linux (amd64)..."
-env GOOS=linux GOARCH=amd64 go build -o bin/linux/amd64/no-role-kick
+env GOOS=linux GOARCH=amd64 go build -o bin/no-role-kick-linux-amd64
 
 cowsay "Building Windows (amd64)..."
-env GOOS=windows GOARCH=amd64 go build -o bin/windows/amd64/no-role-kick.exe
+env GOOS=windows GOARCH=amd64 go build -o bin/no-role-kick-windows-amd64.exe
 
 cowsay "Building MacOS (amd64)..."
-env GOOS=darwin GOARCH=amd64 go build -o bin/darwin/amd64/no-role-kick
+env GOOS=darwin GOARCH=amd64 go build -o bin/darwin/no-role-kick-darwin-amd64
 
 cowsay "Build complete!"
